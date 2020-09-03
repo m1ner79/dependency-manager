e66168885b95b75f2d206a098e81d22aea22a4d8
m1ner79 <michal.d.gornicki@gmail.com>
2020-09-03 13:03:27 +0100

    add eslint and code fix

--- dist/index.js
+++ dist/index.js
@@ -5855,23 +5855,24 @@ const github = __webpack_require__(438);
 const core = __webpack_require__(186);
 
 async function run() {
-    const gitHubKey = process.env.GITHUB_TOKEN || core.getInput('github_token', { required: true });
-    const octokit = github.getOctokit(gitHubKey);
-    const query = "@asyncapi/generator user:asyncapi";
-    
-    // const { data: pullRequest } = await octokit.pulls.get({
-    //     owner: 'octokit',
-    //     repo: 'rest.js',
-    //     pull_number: 123,
-    //     mediaType: {
-    //       format: 'diff'
-    //     }
-    // });
-    const response = await octokit.search.repos({
-        q:query
-      });
+  const gitHubKey = process.env.GITHUB_TOKEN || core.getInput('github_token', { required: true });
+  const octokit = github.getOctokit(gitHubKey);
+  //const query = "user%3Am1ner79+m1ner_test_package&type=Code";
+  const query1 = '"asyncapi/html-template" in:file';
 
-    console.log(response);
+  // const { data: pullRequest } = await octokit.pulls.get({
+  //     owner: 'octokit',
+  //     repo: 'rest.js',
+  //     pull_number: 123,
+  //     mediaType: {
+  //       format: 'diff'
+  //     }
+  // });
+  const response = await octokit.search.code({
+    q: query1
+  });
+
+  console.log(response);
 }
 
 run();
--- index.js
+++ index.js
@@ -2,23 +2,24 @@ const github = require('@actions/github');
 const core = require('@actions/core');
 
 async function run() {
-    const gitHubKey = process.env.GITHUB_TOKEN || core.getInput('github_token', { required: true });
-    const octokit = github.getOctokit(gitHubKey);
-    const query = "@asyncapi/generator user:asyncapi";
-    
-    // const { data: pullRequest } = await octokit.pulls.get({
-    //     owner: 'octokit',
-    //     repo: 'rest.js',
-    //     pull_number: 123,
-    //     mediaType: {
-    //       format: 'diff'
-    //     }
-    // });
-    const response = await octokit.search.repos({
-        q:query
-      });
+  const gitHubKey = process.env.GITHUB_TOKEN || core.getInput('github_token', { required: true });
+  const octokit = github.getOctokit(gitHubKey);
+  //const query = "user%3Am1ner79+m1ner_test_package&type=Code";
+  const query1 = '"asyncapi/html-template" in:file';
 
-    console.log(response);
+  // const { data: pullRequest } = await octokit.pulls.get({
+  //     owner: 'octokit',
+  //     repo: 'rest.js',
+  //     pull_number: 123,
+  //     mediaType: {
+  //       format: 'diff'
+  //     }
+  // });
+  const response = await octokit.search.code({
+    q: query1
+  });
+
+  console.log(response);
 }
 
 run();
