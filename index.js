12a576c8b26fd49800fff0861b08134febeda1f0
m1ner79 <michal.d.gornicki@gmail.com>
2020-09-03 10:10:01 +0100

    adding query

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello, World!");
+const github = require('@actions/github');
+const core = require('@actions/core');
+
+async function run() {
+    const gitHubKey = process.env.GITHUB_TOKEN || core.getInput('github_token', { required: true });
+    const octokit = github.getOctokit(gitHubKey);
+    const query = "@asyncapi/generator user:asyncapi";
+    
+    // const { data: pullRequest } = await octokit.pulls.get({
+    //     owner: 'octokit',
+    //     repo: 'rest.js',
+    //     pull_number: 123,
+    //     mediaType: {
+    //       format: 'diff'
+    //     }
+    // });
+    const response = await octokit.search.repos({
+        q:query
+      });
+
+    console.log(response);
+}
+
+run();
